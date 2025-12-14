92cc4b81f65360877039475865d0b084e8d7c20e
Julian Rendon <juli.rendonn@gmail.com>
2025-12-13 19:25:34 -0500

    Switch to client-side prospect loading with query params

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   integrations: [solidJs(), tailwind()],
-  output: 'hybrid',
-  adapter: vercel({
-    runtime: 'nodejs20.x'
-  }),
   vite: {
     resolve: {
       alias: {
