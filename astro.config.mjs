d4016fd05e4931981ab52e1c1f8060eb8a0564c5
Julian Rendon <juli.rendonn@gmail.com>
2025-12-13 23:24:35 -0500

    Add authentication check to admin dashboard initialization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
-  integrations: [solidJs(), tailwind()],
+  output: 'static',
+  integrations: [solidJs(), tailwind(), vercel()],
   vite: {
     resolve: {
       alias: {
